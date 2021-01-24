b9ad820b5c58b41b512c315084248880006a268b
htmlprog-no <nair.rahul992@gmail.com>
2021-01-23 16:21:48 -0800

    barebones

---
+++ GUI.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import matplotlib.pyplot as plt
+import sympy
+import numpy as np
+
+window = tk.Tk()
+
+title = tk.Label(text="PyGrapher", font=("Consolas", 20))
+
+window.mainloop()
